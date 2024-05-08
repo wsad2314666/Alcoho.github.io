f4854866ad9572546e08be75b00245993cf64562
wsad2314666 <85087356+wsad2314666@users.noreply.github.com>
2024-05-08 21:14:41 +0800

    0508

--- 語音評分系統.py
+++ 語音評分系統.py
@@ -1,7 +1,29 @@
-import pesq
+import pyaudio
+import wave
 
-#載入原始音訊和測試音訊
-ref_audio='original.wav'
-test_audio='test.wav'
-score=pesq.pesq(ref_audio,test_audio,'wb')
-print('PESQ Score:',score)
+CHUNK=1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+
+p =pyaudio.PyAudio()
+stream = p.open(format=FORMAT,channels=CHANNELS,rate=RATE,input=True,frames_per_buffer=CHUNK)
+print("Start recording...")
+
+frames = []
+seconds = 3
+
+for i in range(0,int(RATE/CHUNK*seconds)):
+	data = stream.read(CHUNK)
+	frames.append(data)
+print("reconding stopped")
+
+stream.stop_stream()
+stream.close()
+
+wf=wave.open("output.wav",'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
